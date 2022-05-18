e27dbbbee474642c44feac57fcafbfc8133be69a
Mike <czech.hudek@gmail.com>
2022-05-18 23:32:51 +0300

    3rd

---
+++ HW_6/main.js
@@ -0,0 +1,16 @@
+function checking () {
+    let num;
+    let flag = true;
+    for (let i=2; i<num; i++) {
+        if (num%1 == 0) {
+            flag = false;
+            break;
+        
+        }
+        else if (isNan(num)) {
+            
+        }
+    }
+    
+};
+
